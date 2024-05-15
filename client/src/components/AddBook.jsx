f1271c3bc36d9d9383b71c3aa51f41195bccdc5c
Hadis <sedaghat.hadis@gmail.com>
2024-05-15 15:36:47 -0400

    modified folder

--- client/src/components/AddBook.jsx
+++ client/src/components/AddBook.jsx
@@ -1,23 +1,50 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react'
+import axios from 'axios'
+import  {useNavigate} from 'react-router-dom'
 
-const BookCard = ({book, role}) => {
-    const {name, author, imageUrl} = book;
+const AddBook = () => {
+    const [name, setName] = useState('')
+    const [author, setAuthor] = useState('')
+    const [imageUrl, setImageUrl] = useState('')
+    const navigate = useNavigate()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        axios.post('http://localhost:3001/book/add', {name, author, imageUrl})
+        .then(res => { 
+            if(res.data.added) {
+                navigate('/books')
+            }
+            else {
+                console.log(res)
+            }
+        })
+        .catch(err => console.log(err))
+      }
+
   return (
-    <div className='book-card'>
-        <img src={imageUrl} alt={name} className='book-image'/>
-        <div className="book-details">
-            <h3>{name}</h3>
-            <p>{author}</p>
+    <div className="student-form-container">
+      <form className="student-form" onSubmit={handleSubmit}>
+        <h2>Add Book</h2>
+        <div className="form-group">
+          <label htmlFor="book">Book Name:</label>
+          <input type="text" id="book" name="book" 
+          onChange={(e) => setName(e.target.value)}/>
         </div>
-        {role === "admin" &&
-        <div className="book-actions">
-        <button><Link to={`/book/${book._id}`} className='btn-link'>edit</Link></button>
-        <button><Link to={`/delete/${book._id}`} className='btn-link'>delete</Link></button>
-    </div>}
-        
+        <div className="form-group">
+          <label htmlFor="author">Author Name:</label>
+          <input type="text" id="author" name="author" 
+          onChange={(e) => setAuthor(e.target.value)}/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="image">Image URL:</label>
+          <input type="text" id="image" name="image" 
+          onChange={(e) => setImageUrl(e.target.value)}/>
+        </div>
+        <button type="submit">Add </button>
+      </form>
     </div>
   )
 }
 
-export default BookCard
+export default AddBook
--- server/index.js
+++ server/index.js
@@ -34,5 +34,5 @@ app.get('/dashboard', async (req, res) => {
 })
  
 app.listen(process.env.PORT, () => {
-    console.log("Serve is Running");
+    console.log("Server is Running");
 })
